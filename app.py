3e8923405b5967b6f896fe47dfe403f7a26513ef
dalvipiyush07 <piyushdalvi65>
2025-11-08 13:02:38 +0530

    added

--- app.py
+++ app.py
@@ -1355,5 +1355,5 @@ def newsletter():
     return jsonify({'status': 'success', 'message': 'Newsletter subscription successful'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
